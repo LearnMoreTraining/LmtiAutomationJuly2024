2a3a21db4992fc03a3607b01bc78cb825d87e89a
LearnMoreTraining <anaravinthh@outlook.com>
2024-07-29 22:04:09 +0530

    FRAMEWORK configuration

--- LmtiAutomationMDU-K/src/main/java/utility/BrowserBase.java
+++ LmtiAutomationMDU-K/src/main/java/utility/BrowserBase.java
@@ -1,8 +1,16 @@
 package utility;
 
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 
 public class BrowserBase {
@@ -10,9 +18,24 @@ public class BrowserBase {
    public WebDriver driver ;
 
     public WebDriver getDriver() throws IOException {
+        File f = new File("src/main/resources/configurations/frameworkconfig.properties");
+        FileInputStream fis = new FileInputStream(f);
+        Properties prob = new Properties();
+        prob.load(fis);
+        if(prob.getProperty("browser").equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+        }
+        else if (prob.getProperty("browser").equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+        }
+        else if (prob.getProperty("browser").equalsIgnoreCase("safari")){
+            driver = new SafariDriver();
+        }
+        else{
+            throw new InvalidArgumentException("enter valid browser");
+        }
 
-        driver = new ChromeDriver();
-        driver.get(" https://jqueryui.com/droppable/");
+        driver.get(prob.getProperty("testenvironemnt"));
         return driver;
     }
 }
--- LmtiAutomationMDU-K/src/test/java/runner/FeatureRunner.java
+++ LmtiAutomationMDU-K/src/test/java/runner/FeatureRunner.java
@@ -9,7 +9,10 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
        features = {"src/test/resources/featurefile"},
        glue = {"stepdefinition"},
-       plugin = {"pretty"},
+        plugin = {"pretty",
+                "html:target/test-output/report.html",
+                "json:target/test-output/report.json",
+                "junit:target/test-output/junitreport.xml"},
        tags = "@JqueryO1"
 )
 
